13ed7652fadd32a448b2c6b80c360a0148bec8cf
thucnguyen <thucnguyen11111@gmail.com>
2020-03-16 17:37:01 +0700

    first commit

---
+++ app/components/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import routes from '../constants/routes.json';
+import styles from './Home.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container} data-tid="container">
+      <h2>Home</h2>
+      <div>
+      <Link to={routes.COUNTER}>to Counter</Link>
+      </div>
+      <div>
+      <Link to={routes.SCREEN_STREAM}>to Screen Stream</Link>
+      </div>
+    </div>
+  );
+}
---
+++ app/components/Screen_stream/Screen_stream.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Screen_stream.css';
+
+export default function Screen_stream() {
+    return (
+    <div className= {styles.container}>\
+    <h2>ABC</h2>
+    </div>
+    );
+}
